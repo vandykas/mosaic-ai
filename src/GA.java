4196f66dd811250632b5f8fbaf44006cfaa02f31
marc <6182301062@student.unpar.ac.id>
2026-01-18 21:29:31 +0700

    fix imports

--- src/GA.java
+++ src/GA.java
@@ -1,6 +1,9 @@
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
 
 /**
  * Kelas utama dari Algoritma Genetika.
